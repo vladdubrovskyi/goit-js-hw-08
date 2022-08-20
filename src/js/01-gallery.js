748797ea5ac1d1f048c90e108d197f141d4f9c67
vladdubrovskyi <vladdubrovskyi1993@gmail.com>
2022-08-20 10:35:37 +0300

    working with task 2

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,6 +2,27 @@
 import { galleryItems } from './gallery-items';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
-// Change code below this line
 
-console.log(galleryItems);
+// Change code below this line
+const galleryContainer = document.querySelector('.gallery');
+const galleryMarkup = createGalleryMarkup(galleryItems);
+
+function createGalleryMarkup(items) {
+  const markup = items
+    .map(({ preview, original, description }) => {
+      return `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>
+`;
+    })
+    .join('');
+
+  return markup;
+}
+
+galleryContainer.insertAdjacentHTML('beforeend', galleryMarkup);
+
+const gallery = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
